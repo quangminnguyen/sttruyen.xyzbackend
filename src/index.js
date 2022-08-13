25a75a4c3dc72d57cf601098f939d418ab71f9e1
QuanMinNgu <quangminhnguywn265@gmail.com>
2022-08-13 09:29:54 +0700

    change

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const router = require('./routes/index');
+const Products = require('./models/productController');
+const Chapters = require('./models/chapterController');
 
 dotenv.config();
 app.use(express.json());
@@ -17,16 +19,61 @@ app.use(cors({
 const http = require('http').createServer(app);
 const io = require('socket.io')(http,{
     cors:{
-        origin:[],
+        origin:['http://localhost:3000'],
         methods: ["GET", "POST"],
         allowedHeaders: ["my-custom-header"],
         credentials: true
     }
 });
 
-let users = [];
 io.on('connection',socket => {
 
+    socket.on('readingComic',async (item) => {
+        console.log(item)
+        const product = await Products.findOne({slug:item.slug});
+        if(product){
+            product.watching = product.watching + 1;
+            await Products.findOneAndUpdate({slug:item.slug},{
+                watching:product.watching
+            });
+            const numberChapter = item.chapter.split("_")[1] * 1;
+            const chapterId = product.chapter[numberChapter - 1]._id;
+            const chapter = await Chapters.findById(chapterId);
+            chapter.watching = chapter.watching + 1;
+            await Chapters.findByIdAndUpdate(chapterId,{
+                watching:chapter.watching
+            });
+        }
+    })
+    socket.on('likes',async (item) => {
+        const product = await Products.findOne({slug:item.slug});
+        if(item.status){
+            product.like = product.like + 1;
+        }
+        else{
+            product.like = product.like - 1;
+        }
+        await Products.findOneAndUpdate({slug:item.slug},{
+            like:product.like
+        })
+    });
+
+    socket.on('Rating',async (item) => {
+        const product = await Products.findOne({slug:item.slug});
+        if(product){
+            if(!item.status){
+                product.reviewer += 1;
+                product.rating = product.rating + item.star;
+            }
+            else{
+                product.rating = product.rating - item.oldStar + item.star;
+            }
+            await Products.findOneAndUpdate({slug:item.slug},{
+                rating:product.rating,
+                reviewer:product.reviewer
+            })
+        }
+    });
 })
 
 mongoose.connect(process.env.DATABASE_URL,{
